e467e9dd28ce433bde0b925f5e605ce93ff49020
Alberto Barrajón <tolletum@gmail.com>
2019-11-25 22:35:34 +0100

    feat(java): added filter for cors

---
+++ coordinator/src/main/java/com/cdnator/coordinator/AddResponseHeaderFilter.java
@@ -0,0 +1,37 @@
+package com.cdnator.coordinator;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+
+@WebFilter("/coordinators/*")
+public class AddResponseHeaderFilter implements Filter {
+
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+      throws IOException, ServletException {
+    HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+    httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
+    httpServletResponse.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Origin");
+
+    chain.doFilter(request, response);
+  }
+
+  @Override
+  public void init(FilterConfig filterConfig) throws ServletException {
+    // ...
+  }
+
+  @Override
+  public void destroy() {
+    // ...
+  }
+
+}
