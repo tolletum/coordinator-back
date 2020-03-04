7896072a0a1a1c33967c0ef1c0d0713474475a27
alberto.barrajon.next <alberto.barrajon.next@bbva.com>
2020-03-04 08:41:26 +0100

    feat: add constructor with params

--- coordinator/src/main/java/com/cdnator/coordinator/dto/HoursQIdDTO.java
+++ coordinator/src/main/java/com/cdnator/coordinator/dto/HoursQIdDTO.java
@@ -2,18 +2,26 @@ package com.cdnator.coordinator.dto;
 
 import com.cdnator.coordinator.dto.enumtypes.MonthEnum;
 import com.cdnator.coordinator.dto.enumtypes.QuarterEnum;
+import ch.qos.logback.core.joran.spi.NoAutoStart;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 @Data
+@RequiredArgsConstructor
 @NoArgsConstructor
 public class HoursQIdDTO {
 
+    @NonNull
     private String year;
 
+    @NonNull
     private QuarterEnum quarter;
 
+    @NonNull
     private MonthEnum month;
 
+    @NonNull
     private EmployeeDTO employee;
 }
